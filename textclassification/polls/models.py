5f7649f805e67b7b6e584ba6cdf3f0ac2ad973e3
zeshanbaig786 <zeshanbaig786@gmail.com>
2022-12-02 22:38:20 +0500

    till part6 done

--- textclassification/polls/models.py
+++ textclassification/polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+import datetime
 
 
 class Question(models.Model):
@@ -10,7 +11,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
